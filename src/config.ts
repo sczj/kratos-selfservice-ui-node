6ecb16831452c44d4ef5bd3a69d60c1a0c5b0885
sczj <910090051@qq.com>
2020-02-26 10:13:51 +0800

    as

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export default {
   kratos: {
-    browser: (process.env.KRATOS_BROWSER_URL || '').replace(/\/+$/, ''),
-    admin: (process.env.KRATOS_ADMIN_URL || '').replace(/\/+$/, ''),
-    public: (process.env.KRATOS_PUBLIC_URL || '').replace(/\/+$/, ''),
+    browser: (process.env.KRATOS_BROWSER_URL || 'http://127.0.0.1:1235').replace(/\/+$/, ''),
+    admin: (process.env.KRATOS_ADMIN_URL || 'http://127.0.0.1:1234/').replace(/\/+$/, ''),
+    public: (process.env.KRATOS_PUBLIC_URL || 'http://127.0.0.1:1235/').replace(/\/+$/, ''),
   },
   baseUrl: (process.env.BASE_URL || '/').replace(/\/+$/, '') + '/',
   jwksUrl: process.env.JWKS_URL || '/',
